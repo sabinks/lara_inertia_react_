95a0852bf007dfb4c60b04ab36518063529f877b
sabinks <sabin.kr.stha@gmail.com>
2024-05-26 14:35:18 +0545

    initial setup for laravel react inertia for booking app

---
+++ resources/js/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Head } from '@inertiajs/react'
+function Home() {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.{html,jsx,tsx}",
+        "./resources/views/*.blade.php"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
